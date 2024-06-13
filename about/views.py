4f30a57e41874e69e3ae4680a7222b3aae09e8c6
thomas wharton <thomaswharton7@hotmail.com>
2024-06-13 15:43:10 +0100

    Added comment CRUD

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -8,6 +9,15 @@ def about_me(request):
     """
     Renders the About page
     """
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Your collaboration request has been submitted'
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
