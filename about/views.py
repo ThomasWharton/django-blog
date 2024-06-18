7ecc64edcb3d9925813f8f1bf7f687391035c934
Thomas <thomaswharton7@hotmail.com>
2024-06-18 14:48:40 +0100

    Remove unneccessary import

--- about/views.py
+++ about/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
